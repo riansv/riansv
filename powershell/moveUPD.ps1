6ef6b17ddf1d1af4a77958ba511d9b41b7cbe8ea
Ciprian Ci <ciprian.ciuhan@outlook.com>
2020-04-15 09:19:20 +0300

    import users from file

--- powershell/moveUPD.ps1
+++ powershell/moveUPD.ps1
@@ -1,34 +1,44 @@
 #pre-requisites
 import-module ActiveDirectory
+$scriptlocation = $PSScriptRoot
 
-#getting info needed to copy data
-$user = Read-Host "What is your username?"
-$sid = (Get-ADUser $user).sid.value
+#start of script
 
-$vhdpath = "\\vmhrmweuprdfs03\UPDv2\UVHD-$sid.vhdx"
+foreach($user in (get-content "$scriptlocation\users.txt")){
 
-#mounting the VHD without letter and getting the disk number
+    try{
+        #getting info needed to copy data
+        $sid = (Get-ADUser $user).sid.value
 
-$mountedDisk = (Mount-DiskImage -ImagePath $VHDPath -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage).Number
+        $vhdpath = "\\vmhrmweuprdfs03\UPDv2\UVHD-$sid.vhdx"
+
+        #mounting the VHD without letter and getting the disk number
+
+        $mountedDisk = (Mount-DiskImage -ImagePath $VHDPath -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage).Number
 
 
-#creating a temporary folder for mount point
-New-Item -Path C:\Temp -ItemType Directory -Name $user
+        #creating a temporary folder for mount point
+        New-Item -Path C:\Temp -ItemType Directory -Name $user -ErrorAction Stop
 
-#creating a mount point based on disknumber
-Add-PartitionAccessPath -DiskNumber $mountedDisk -PartitionNumber 1 -AccessPath "C:\Temp\$user"
+        #creating a mount point based on disknumber
+        Add-PartitionAccessPath -DiskNumber $mountedDisk -PartitionNumber 1 -AccessPath "C:\Temp\$user" -ErrorAction Stop
 
-#copy everything that is in UPD under AppData\Roaming to Redirected Appdata
-robocopy C:\Temp\$user\appdata\Roaming \\vmhrmweuprdfs03\Folderredirection\$user\appdata\Roaming /MIR /COPYALL
+        #copy everything that is in UPD under AppData\Roaming to Redirected Appdata
+        robocopy C:\Temp\$user\appdata\Roaming \\vmhrmweuprdfs03\Folderredirection\$user\appdata\Roaming /MIR /COPYALL
 
-#copy startmenu from shared location to user profile Start Menu
-robocopy \\vmhrmweuprdfs03\UPDv2\redirection\startmenu \\vmhrmweuprdfs03\Folderredirection\$user\Startmenu /MIR /COPYALL
-a
-#remove mount point
-Remove-PartitionAccessPath -DiskNumber $mountedDisk -PartitionNumber 1 -AccessPath "C:\Temp\$user"
+        #copy startmenu from shared location to user profile Start Menu
+        robocopy \\vmhrmweuprdfs03\UPDv2\redirection\startmenu \\vmhrmweuprdfs03\Folderredirection\$user\Startmenu /MIR /COPYALL
 
-#dismount vhd
-Dismount-DiskImage -ImagePath $VHDPath
+        #remove mount point
+        Remove-PartitionAccessPath -DiskNumber $mountedDisk -PartitionNumber 1 -AccessPath "C:\Temp\$user"
 
-#delete folder previously created
-remove-item -Path C:\Temp\$user
+        #dismount vhd
+        Dismount-DiskImage -ImagePath $VHDPath
+
+        #delete folder previously created
+        remove-item -Path C:\Temp\$user
+    }
+    Catch {
+        Write-host "$user couldn't be migrated"
+    }
+}

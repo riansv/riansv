511fe4bd8e02bd30f35c5d286573e6ad514e6de7
Ciprian Ci <ciprian.ciuhan@outlook.com>
2020-04-16 09:13:38 +0300

    add permission

--- powershell/moveUPD.ps1
+++ powershell/moveUPD.ps1
@@ -1,20 +1,20 @@
 #pre-requisites
 import-module ActiveDirectory
 $scriptlocation = $PSScriptRoot
-
+remove-item $PSScriptRoot\issues.txt
 #start of script
 
 foreach($user in (get-content "$scriptlocation\users.txt")){
 
     try{
         #getting info needed to copy data
-        $sid = (Get-ADUser $user).sid.value
+        $sid = (Get-ADUser $user).sid.value 
 
         $vhdpath = "\\vmhrmweuprdfs03\UPDv2\UVHD-$sid.vhdx"
 
         #mounting the VHD without letter and getting the disk number
 
-        $mountedDisk = (Mount-DiskImage -ImagePath $VHDPath -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage).Number
+        $mountedDisk = (Mount-DiskImage -ImagePath $VHDPath -NoDriveLetter -PassThru -ErrorAction Stop | Get-DiskImage).Number 
 
 
         #creating a temporary folder for mount point
@@ -27,7 +27,11 @@ foreach($user in (get-content "$scriptlocation\users.txt")){
         robocopy C:\Temp\$user\appdata\Roaming \\vmhrmweuprdfs03\Folderredirection\$user\appdata\Roaming /MIR /COPYALL
 
         #copy startmenu from shared location to user profile Start Menu
-        robocopy \\vmhrmweuprdfs03\UPDv2\redirection\startmenu \\vmhrmweuprdfs03\Folderredirection\$user\Startmenu /MIR /COPYALL
+        robocopy \\vmhrmweuprdfs03\UPDv2\redirection\startmenu "\\vmhrmweuprdfs03\Folderredirection\$user\Start Menu" /MIR /COPYALL
+
+       #configure full access over Start Menu folder
+        $rights = 'icacls "\\vmhrmweuprdfs03\Folderredirection\'+$user+'\Start Menu" /grant '+$user+':F /T'
+        Invoke-Expression $rights
 
         #remove mount point
         Remove-PartitionAccessPath -DiskNumber $mountedDisk -PartitionNumber 1 -AccessPath "C:\Temp\$user"
@@ -39,6 +43,8 @@ foreach($user in (get-content "$scriptlocation\users.txt")){
         remove-item -Path C:\Temp\$user
     }
     Catch {
-        Write-host "$user couldn't be migrated"
+        "$user couldn't be migrated" | Out-file $scriptlocation\issues.txt -Append
     }
 }
+
+pause
